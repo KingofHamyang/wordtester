2e38741cbfee2256c17f77aa5420e8a6587e416f
KingofHamyang <kjds1235@naver.com>
2019-02-10 17:07:00 +0900

    dictionary link added

--- app.js
+++ app.js
@@ -16,36 +16,41 @@ app.get('/', (req, res) => {
 })
 
 app.get('/wordparser', (req, res) => {
+
     var fs = require('fs');
     var array = fs.readFileSync('wordpool.txt').toString().split("\n");
     var cnt = 0;
     var wordcnt = new Array();
     var wordspace = new Array();
 
+
+
     for (i in array) {
         wordcnt[i] = 0;
 
         cnt++;
     }
+    if (req.query.number <= cnt) {
 
-    for (var i = 0; i < req.query.number; i++) {
-        var index = (Math.floor(Math.random() * (cnt + 1)));
-        if (wordcnt[i] != 0) {
-            i--;
-            continue;
-        } else {
-            wordcnt[i] = 1;
-            wordspace.push(array[index])
+        for (var i = 0; i < req.query.number; i++) {
+            var index = (Math.floor(Math.random() * (cnt + 1)));
+            if (wordcnt[i] != 0) {
+                i--;
+                continue;
+            } else {
+                wordcnt[i] = 1;
+                wordspace.push(array[index])
+            }
         }
+
+        console.log(wordspace.length)
+        console.error(req.query.number)
+
+
+    } else {
+        wordspace.push("pool에 있는 단어 수보다 적게 입력해주세요 ! 현재 pool의 단어 수는 " + cnt + "개 입니다.")
+        req.query.number = 1;
     }
-    for (i in wordspace) {
-
-        console.log(wordspace[i]);
-
-    }
-    console.log(wordspace.length)
-    console.error(req.query.number)
-
     res.render('wordparser', {
         worddata: wordspace,
         cnt: req.query.number
